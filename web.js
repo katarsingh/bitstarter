5e65c126c2d94a051095b61991cc525392748186
Katar.Singh <katar_singh@yahoo.com>
2013-07-12 03:32:43 +0000

    added web.as 2

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
+fs = require('fs') ;
+require('buffer') ;
+
+app.get('/',function(request, response) {
+
+var buf = fs.readFileSync('index.html','utf8') ;
+response.send(buf) ;
 });
 
 var port = process.env.PORT || 5000;
